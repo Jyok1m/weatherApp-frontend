81ff1a8747581ec3f9b0f7406094aac02a2b0f68
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-01-23 13:03:21 +0100

    maybe

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ document.querySelector('#register').addEventListener('click', function () {
 		password: document.querySelector('#registerPassword').value,
 	}
 
-	fetch('https://weather-app-backend-pi.vercel.app/users/signup', {
+	fetch('https://weather-app-backend-olive.vercel.app/users/signup', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(user),
@@ -26,7 +26,7 @@ document.querySelector('#connection').addEventListener('click', function () {
 		password: document.querySelector('#connectionPassword').value,
 	}
 
-	fetch('https://weather-app-backend-pi.vercel.app/users/signin', {
+	fetch('https://weather-app-backend-olive.vercel.app/users/signin', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(user),
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://weather-app-backend-pi.vercel.app/weather')
+fetch('https://weather-app-backend-olive.vercel.app/weather')
 	.then(response => response.json())
 	.then(data => {
 		if (data.weather && data.currentPosWeather) {
@@ -39,7 +39,7 @@ fetch('https://weather-app-backend-pi.vercel.app/weather')
 function updateDeleteCityEventListener() {
 	for (let i = 0; i < document.querySelectorAll('.deleteCity').length; i++) {
 		document.querySelectorAll('.deleteCity')[i].addEventListener('click', function () {
-			fetch(`https://weather-app-backend-pi.vercel.app/weather/${this.id}`, { method: 'DELETE' })
+			fetch(`https://weather-app-backend-olive.vercel.app/weather/${this.id}`, { method: 'DELETE' })
 				.then(response => response.json())
 				.then(data => {
 					if (data.result) {
@@ -53,7 +53,7 @@ function updateDeleteCityEventListener() {
 document.querySelector('#addCity').addEventListener('click', function () {
 	const cityName = document.querySelector('#cityNameInput').value;
 
-	fetch('https://weather-app-backend-pi.vercel.app/weather', {
+	fetch('https://weather-app-backend-olive.vercel.app/weather', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({ cityName }),
